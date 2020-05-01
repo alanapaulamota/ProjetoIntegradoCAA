8e923ffdba61713bc9226daf4282d0a49cda328c
alanapaulamota <alanapaula.job@gmail.com>
2020-05-01 03:13:28 -0300

    add testes...

---
+++ src/test/java/br/com/alana/projetoIntegrado/CAA/Resouce/UsuarioResouceIT.java
@@ -0,0 +1,14 @@
+package br.com.alana.projetoIntegrado.CAA.Resouce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UsuarioResouceIT {
+
+	@Test
+	public void testFindById() {
+
+	}
+
+}
---
+++ src/test/java/br/com/alana/projetoIntegrado/CAA/Resouce/UsuarioResouceTest.java
@@ -0,0 +1,24 @@
+package br.com.alana.projetoIntegrado.CAA.Resouce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UsuarioResouceTest {
+
+	@Test
+	public void testFindByIdErroIdInvalido() {
+
+	}
+
+	@Test
+	public void testFindByIdErroIdEmBranco() {
+
+	}
+
+	@Test
+	public void testFindByIdErroIdNulo() {
+
+	}
+
+}
